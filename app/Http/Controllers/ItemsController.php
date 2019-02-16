a8bb42770397358834ec718c96acbf1054f82c8c
Shovon Das <shovondas26@gmail.com>
2019-02-16 18:33:07 +0600

    Get & Post Request Done , CSRF Disabled for Api/*

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+use Validator;
+class ItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }   
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $this ->validate($request, [
+        //     'text' => 'required'
+        // ]);
+
+        $validator = Validator::make($request->all(), [
+            'text' => 'required'
+        ]);
+
+        if($validator->fails()){
+            //return 'fails';
+             $response = array('response' => $validator ->messages(), 'success' => false);
+             return $response;
+        }else{
+            //Create Item
+            $item = new Item;
+            $item -> text = $request -> input('text');
+            $item -> body = $request -> input('body');
+            $item -> save();
+
+             return response()->json($item);
+            //return 123;
+        }
+
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
+        return response()->json($item);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    { 
+        //
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+<p align="center"><img src="http://shovon.net/projects/freelancer/DaRose/images/assets/logo.png"></p>
